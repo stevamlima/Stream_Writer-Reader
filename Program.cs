10ed081cea4fd0479c1b866ae9a7854692c704c1
FIC <stevamlima99@gmail.com>
2017-10-11 19:21:00 -0300

    código finalizado

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace project_11_10_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+    
+        {
+            Console.WriteLine("\nQuestionário|");
+            Console.WriteLine("============");
+            StreamWriter resp = new StreamWriter("C:\\Users\\FIC\\Desktop\\respostas.csv",true);
+            string [] perguntas = File.ReadAllLines("C:\\Users\\FIC\\Desktop\\perguntas.txt");
+            string [] respostas = new string[perguntas.Length];
+
+            for(int i=0;i<perguntas.Length;i++)
+            {
+                Console.Write(perguntas[i]+":");
+                respostas[i] = Console.ReadLine();
+                resp.Write(respostas[i]+"; ");
+            }
+            resp.WriteLine();
+            resp.Close();
+
+        }
+    }
+}
